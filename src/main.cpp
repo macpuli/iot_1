154845fbe6565616ddd10859ec201f91f98fca43
Martín Coba <53869350+macpuli@users.noreply.github.com>
2021-11-26 10:14:03 -0600

    Base proyecto

---
+++ src/main.cpp
@@ -0,0 +1,104 @@
+#include <Arduino.h>
+#include <PubSubClient.h>
+#include <WiFi.h>
+int led_gpio = 15;
+
+const char* ssid = "INFICobaPu2.4";
+const char* password = "P1e2Lu10S2a1";
+
+const char *mqtt_server = "driver.cloudmqtt.com";
+const int mqtt_port = 18651;
+const char *mqtt_user = "clxxwiek";
+const char *mqtt_pass = "PtsiiKX9xhVm";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+void callback(char* topic, byte* payload, unsigned int length){
+  Serial.print("Mensaje recibido bajo el tópico ->");
+  Serial.print(topic);
+  Serial.print("\n");
+
+  for (int i=0; i<length; i++){
+    Serial.print((char)payload[i]);
+  }
+
+  if((char)payload[0] == '0'){
+    digitalWrite(led_gpio, LOW);
+    Serial.println("\n LED apagado");
+  }
+  else{
+    digitalWrite(led_gpio, HIGH);
+    Serial.println("\n LED encendido");
+  }
+
+  Serial.println();
+}
+
+void reconnect(){
+  while(!client.connected()){
+    Serial.println("Intentando Conexión MQTT");
+
+    String clientId = "iot_1_";
+    clientId = clientId + String(random(0xffff),HEX);
+
+    if (client.connect(clientId.c_str(), mqtt_user, mqtt_pass)) {
+      Serial.println("Conexión a MQTT exitosa");
+      client.publish("salidaMCP", "Primer mensaje");
+      client.subscribe("entradaMCP");
+    } else {
+      Serial.print("Falló la conexión");
+      Serial.print(client.state());
+      Serial.println("Se intentará de nuevo en 5 segundos");
+      delay(5000);
+    }
+  }
+}
+
+void setup_wifi(){
+  Serial.println();
+  Serial.println("Conectando a...");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("Conectando a red ->");
+  Serial.println("Dirección IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led_gpio, OUTPUT);
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(client.connected()==false){
+    reconnect();
+  }
+
+  client.loop();
+  if(millis()-lastMsg > 2000){
+    lastMsg = millis();
+    value++;
+    String mes = "Valor ->" + String(value);
+    mes.toCharArray(msg, 50);
+    client.publish("MCP", msg);
+    Serial.println("Mensaje enviado ->" + String(value));
+  }
+}
